716852049426f52151b754d2307b0943a4cb6e61
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2020-02-24 18:32:05 +0000

    Add luma_core project

---
+++ luma_core/src/lib.rs
@@ -0,0 +1,5 @@
+//! ``luma_core`` is the core module of ``luma``.
+//! 
+//! This module contains core processor features.
+//! 
+//! **NOTE**: This is currently in a very experimental state and is subject to change.
