eac0794f608cc89df4a2809dca6ee63a1e550a3b
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2020-02-24 17:34:53 +0000

    Add library information

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
-
+//! ``luma`` is a [Rust][rs] rewrite of devkitPro's [libogc][ogc].
+//! 
+//! **NOTE**: This is currently in a very experimental state and is subject to change.
+//!
+//! [rs]: https://www.rust-lang.org/
+//! [ogc]: https://github.com/devkitPro/libogc
