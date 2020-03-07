6752a5c894ad34aee3bdd6bee964829140d04637
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2020-03-07 22:32:24 +0000

    Add more cache functions

--- luma_core/src/cache.rs
+++ luma_core/src/cache.rs
@@ -13,15 +13,47 @@ extern "C" {
     pub fn DCDisable();
     
     /// Invalidate the L1 d-cache.
-    /// 
-    /// **NOTE**: Cache access is blocked during this time.
     pub fn DCFlashInvalidate();
 
     /// Locks the current contents of the L1 d-cache.
-    /// 
-    /// **NOTE**: Most cache operations, will still execute regardless of whether the cache is frozen.
     pub fn DCLock();
 
     /// Unlocks the current contents of the L1 d-cache.
     pub fn DCUnlock();
+
+    /// Invalidates a given range of the L1 d-cache.
+    pub fn DCInvalidateRange(start_address: *const u32, length: u32);
+
+    /// Flushes a given range of the L1 d-cache.
+    pub fn DCFlushRange(start_address: *const u32, length: u32);
+
+    /// Ensures a range of memory is updated with any modified data in the cache.
+    pub fn DCStoreRange(start_address: *const u32, length: u32);
+
+    /// Flushes a given range of the L1 d-cache.
+    pub fn DCFlushRangeNS();
+
+    /// Ensures a range of memory is updated with any modified data in the cache.
+    pub fn DCStoreRangeNS();
+
+    /// Enable the L1 i-cache.
+    pub fn ICEnable();
+
+    /// Disable the L1 i-cache.
+    pub fn ICDisable();
+
+    /// Locks the current contents of the L1 i-cache.
+    pub fn ICLock();
+
+    /// Unlocks the current contents of the L1 i-cache.
+    pub fn ICUnlock();
+
+    /// Invalidate the L1 i-cache.
+    pub fn ICFlashInvalidate();
+
+    /// Invalidates a block in the i-cache.
+    pub fn ICBlockInvalidate(start_address: *const u32);
+
+    /// Invalidate a range in the L1 i-cache.
+    pub fn ICInvalidateRange(start_address: *const u32, length: u32);
 }
