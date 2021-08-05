4b34a696bf0f14cc24655c190ab6050a7dbb68b2
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2021-08-06 00:06:08 +0100

    Remove start function

--- luma_runtime/src/lib.rs
+++ luma_runtime/src/lib.rs
@@ -14,26 +14,6 @@ global_asm!(include_str!("../asm/crt0.S"));
 global_asm!(include_str!("../asm/runtime.S"));
 global_asm!(include_str!("../asm/system.S"));
 
-/// This is the executable start function, which directly follows the entry point.
-#[cfg_attr(not(test), lang = "start")]
-#[cfg(not(test))]
-extern "C" fn start<T>(user_main: *const (), _argc: isize, _argv: *const *const u8) -> !
-where
-    T: Termination,
-{
-    let user_main: fn() -> T = unsafe { core::mem::transmute(user_main) };
-    user_main();
-
-    panic!("main() cannot return");
-}
-
-/// Termination trait required for the start function.
-#[cfg_attr(not(test), lang = "termination")]
-trait Termination {}
-
-/// This implementation does the bare minimum to satisfy the executable start function.
-impl Termination for () {}
-
 /// This function is called on panic.
 #[cfg_attr(not(test), panic_handler)]
 #[no_mangle]
