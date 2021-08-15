0db613954cf4ec1d3848d1c248b25f0dc9b61317
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2021-08-15 15:56:34 +0100

    Rework start function to fix LLVM issue

--- luma_runtime/src/lib.rs
+++ luma_runtime/src/lib.rs
@@ -8,11 +8,33 @@
 #![feature(global_asm, lang_items, llvm_asm)]
 
 use core::panic::PanicInfo;
+#[allow(unused_imports)]
+use luma_core::cache::*;
 
 // crt0 Implementation
 global_asm!(include_str!("../asm/crt0.S"));
 global_asm!(include_str!("../asm/runtime.S"));
 global_asm!(include_str!("../asm/system.S"));
+
+/// This is the executable start function, which directly follows the entry point.
+#[cfg_attr(not(test), lang = "start")]
+#[cfg(not(test))]
+extern "C" fn start<T>(user_main: *const (), _argc: isize, _argv: *const *const u8) -> isize
+where
+    T: Termination,
+{
+    let user_main: fn() -> T = unsafe { core::mem::transmute(user_main) };
+    user_main();
+
+    panic!("main() cannot return");
+}
+
+/// Termination trait required for the start function.
+#[cfg_attr(not(test), lang = "termination")]
+trait Termination {}
+
+/// This implementation does the bare minimum to satisfy the executable start function.
+impl Termination for () {}
 
 /// This function is called on panic.
 #[cfg_attr(not(test), panic_handler)]
@@ -25,16 +47,7 @@ fn panic(_info: &PanicInfo) -> ! {
     }
 }
 
-/// Re-export libc's memset to be used in ``crt0.S``.
-///
-/// # Safety
-///
-/// Refer to the platform's libc implementation for more information.
+/// Error handler personality language item (current no-op, to satisfy clippy).
+#[cfg_attr(not(test), lang = "eh_personality")]
 #[no_mangle]
-pub unsafe extern "C" fn memset(
-    dest: *mut libc::c_void,
-    c: libc::c_int,
-    n: libc::size_t,
-) -> *mut libc::c_void {
-    libc::memset(dest, c, n)
-}
+extern "C" fn rust_eh_personality() {}
