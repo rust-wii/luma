f8ac1dd67ef2b48de10eff4667ab366683112a9c
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2021-11-22 23:22:11 +0100

    Add an example echo bot using the USB Gecko
    This program repeats every byte it is fed.

---
+++ src/bin/echo-bot.rs
@@ -0,0 +1,22 @@
+//! This is an example of how to communicate with an USB Gecko using Luma.
+
+#![no_std]
+
+extern crate luma_core;
+extern crate luma_runtime;
+
+use luma_core::exi::Exi;
+use luma_core::exi::usb_gecko::UsbGecko;
+
+fn main() {
+    let exi = Exi::init();
+    let gecko = UsbGecko::new(&exi).unwrap();
+    loop {
+        // TODO: use interrupts here, instead of a busy loop.
+        let buf = match gecko.receive() {
+            Ok(buf) => buf,
+            Err(_) => continue,
+        };
+        gecko.send(&buf).unwrap();
+    }
+}
