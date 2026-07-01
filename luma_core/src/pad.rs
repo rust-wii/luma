098ff43f7b2e56a72dc5bd281c0f4f7c38c0e1c0
JGM01 <jacobgermanamccray@gmail.com>
2026-06-30 23:25:03 -0400

    testing testing 1 2 3

--- luma_core/src/pad.rs
+++ luma_core/src/pad.rs
@@ -12,10 +12,10 @@ const CMD_READ_STATUS: u32 = 0x0040_0300;
 
 /// A snapshot of the controller's state at a point in time.
 /// - Holds the raw 8-byte payload from the Serial Interface.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub struct PadState {
-    word1: u32,
-    word2: u32,
+    pub word1: u32,
+    pub word2: u32,
 }
 
 impl PadState {
